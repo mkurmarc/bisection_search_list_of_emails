dbc18cbb970451ff6a42ba4636f4e0754d91a43b
Marc Rios <marc.rios1712@gmail.com>
2020-01-29 15:32:10 -0800

    added: user inputs, bisection func, domain name func, runtime analysis

--- bisection_search_emails.py
+++ bisection_search_emails.py
@@ -1,8 +1,37 @@
-from functions import analyze_func, generate_emails, generate_name
+from functions import analyze_func, generate_emails, generate_name, bisection_iter
 
-generate_name(15)
+list_of_domains = ["yohoo.com", "gomail.com", "happymail.com","aol.com"]
 
-list_of_domains = ["yahoo.com", "gmail.com", "hotmail.com","aol.com"]
-email_list_size = int(input("How many emails you want to generate?"))
+# inputs for legnth of name in email and size of email list
+length_names = int(input("How many characters for the name length? "))
+email_list_size = int(input("How many emails you want to generate? "))
 
-print(generate_emails(generate_name(15), list_of_domains, email_list_size))
+# generates email list
+email_arr = generate_emails(length_names, list_of_domains, email_list_size)
+
+# user input for their name to creat email to be inserted into gen email list
+user_name = input("Type in your name to create email:  ")
+implanted_email = user_name + "@yohoo.com"
+
+# inserts user email into list
+print(f"Inserting {implanted_email} into email list...")
+email_arr.append(implanted_email) # add check statement to make sure input email is valid
+
+# sorts list of emails
+sorted_email_arr = sorted(email_arr)
+
+# print(sorted_email_arr) # uncomment to see the array
+
+# runs bisection to find user email within gen email list
+index, found = bisection_iter(implanted_email, sorted_email_arr)
+
+# prints bisection results
+print(found)
+
+# checks to make sure the bisection worked
+if index:
+    print(f"Element at index {index} is {sorted_email_arr[index]}")
+
+# analyzes functions' run times
+analyze_func(generate_emails,length_names, list_of_domains, email_list_size)
+analyze_func(bisection_iter,implanted_email, sorted_email_arr)
--- functions.py
+++ functions.py
@@ -2,33 +2,37 @@ import time
 from random import choice
 from string import ascii_lowercase as letters
 
-# def bisection_iter(n, arr):
-#     start = 0
-#     stop = len(arr)-1
-#     while start <= stop:
-#         mid = (start + stop)//2
-#         if n == arr[mid]:
-#             return f"{n} found at index: {mid}"
-#         elif n > arr[mid]:
-#             start = mid+1
-#         else:
-#             stop = mid-1
-#     return f"{n} not found in list"
+def bisection_iter(email, arr):
+    start = 0
+    stop = len(arr)-1
+    while start <= stop:
+        mid = (start + stop)//2
+        if email == arr[mid]:
+            return mid, f"{email} found at index {mid}"
+        elif email > arr[mid]:
+            start = mid+1
+        else:
+            stop = mid-1
+    return None, f"{email} not found in list"
 
-def analyze_func(func_name, arr):
+def analyze_func(func_name, *args):
     tic = time.time()
-    func_name(arr)
+    func_name(*args)
     toc = time.time()
     seconds = toc - tic
     print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")
 
-def generate_emails(names, list_of_domains, email_list_size):
+################# email generation functions below ##########################
+
+def generate_emails(length_names, list_of_domains, email_list_size):
     list_of_emails = []
     for i in range(email_list_size):
-        email = names + "@" + choice(list_of_domains)
-        list_of_emails.append(email)
-        i += 1
-    return list_of_emails
+        email = generate_name(length_names) + "@" + get_domain_name(list_of_domains)
+        list_of_emails.append(email) # can make shorter by inserting email assignment into append parenthesis
+    return list_of_emails            # did not do that to keep process clear
 
-def generate_name(name_legnth):
-    return "".join(choice(letters) for i in range(name_legnth))
+def get_domain_name(list_of_domains):
+    return choice(list_of_domains)
+
+def generate_name(name_length):
+    return "".join(choice(letters) for i in range(name_length))
